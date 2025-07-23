b591643f919d98efb082946d5ae0080c937bb7a6
viteakira <viteakira@users.noreply.github.com>
2025-07-23 23:55:00 +0300

    Added functions to get computer and human choice

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("Hello World")
+function getComputerChoice () {
+    let value = Math.floor(Math.random() * 3);
+    if (value == 0) {
+        return "rock"
+    }
+    else if (value == 1) {
+        return "paper"
+    }
+    else {
+        return "scissors"
+    }
+}
+
+console.log(getComputerChoice());
+
+function getHumanChoice () {
+    let choice = prompt("Rock, paper or scissors? :");
+    return choice
+}
+
+console.log(getHumanChoice());
+
+let humanScore = 0;
+let computerScore = 0;
+
+function (comp, human) {
+    // TODO
+}
